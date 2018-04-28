49dc4f62d06cf2a6859326432a4e540c826508b7
Diep Pham <me@favadi.com>
2018-04-28 12:17:16 +0700

    refactors tag items injecting

--- parse.go
+++ parse.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func tagFromComment(comment string) (tag string) {
 	match := rComment.FindStringSubmatch(comment)
@@ -10,10 +13,61 @@ func tagFromComment(comment string) (tag string) {
 	return
 }
 
+type tagItem struct {
+	key   string
+	value string
+}
+
+type tagItems []tagItem
+
+func (ti tagItems) format() string {
+	tags := []string{}
+	for _, item := range ti {
+		tags = append(tags, fmt.Sprintf(`%s:%s`, item.key, item.value))
+	}
+	return strings.Join(tags, " ")
+}
+
+func (ti tagItems) override(nti tagItems) tagItems {
+	overrided := []tagItem{}
+	for i := range ti {
+		var dup = -1
+		for j := range nti {
+			if ti[i].key == nti[j].key {
+				dup = j
+				break
+			}
+		}
+		if dup == -1 {
+			overrided = append(overrided, ti[i])
+		} else {
+			overrided = append(overrided, nti[dup])
+			nti = append(nti[:dup], nti[dup+1:]...)
+		}
+	}
+	return append(overrided, nti...)
+}
+
+func newTagItems(tag string) tagItems {
+	items := []tagItem{}
+	splitted := strings.Split(tag, " ")
+	for _, t := range splitted {
+		sepPos := strings.Index(t, ":")
+		items = append(items, tagItem{
+			key:   t[:sepPos],
+			value: t[sepPos+1:],
+		})
+	}
+	return items
+}
+
 func injectTag(contents []byte, area textArea) (injected []byte) {
 	expr := make([]byte, area.End-area.Start)
 	copy(expr, contents[area.Start-1:area.End-1])
-	expr = rInject.ReplaceAll(expr, []byte(fmt.Sprintf("`%s %s`", area.CurrentTag, area.InjectTag)))
+	cti := newTagItems(area.CurrentTag)
+	iti := newTagItems(area.InjectTag)
+	ti := cti.override(iti)
+	expr = rInject.ReplaceAll(expr, []byte(fmt.Sprintf("`%s`", ti.format())))
 	injected = append(injected, contents[:area.Start-1]...)
 	injected = append(injected, expr...)
 	injected = append(injected, contents[area.End-1:]...)
